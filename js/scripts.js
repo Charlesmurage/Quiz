0652296cb37a6339055de80bfaa857e7dd0daa46
Charles maina <muragemaina09@gmail.com>
2019-03-29 12:13:12 +0300

    added code to the scripts.js page

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+if(no1==="yes") {
+    return 2;
+}
+else if(no2==="stack") {
+    return 2;
+}
+else if(no3==="netscape") {
+    return 2;
+}
+else if(no4==="else") {
+    return 2;
+}
+else if(no5==="appear") {
+    return 2;
+}
+else if(no6==="yes") {
+    return 2;
+}
+else if(no7==="=") {
+    return 2;
+}
+else if(no8==="area") {
+    return 2;
+}
+else if(no9==="logical error") {
+    return 2;
+}
+else if(no10==="no") {
+    return 2;
+}
+else {
+    return 0;
+}
+var result=function(no1, no2, no3, no4, no5, no6, no7, no8, no9, no10); {
+return no1+no2+no3+no4+no5+no6+no7+no8+no9+no10;
+}
