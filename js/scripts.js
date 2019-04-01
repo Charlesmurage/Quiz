b4307673501dbe5f9133675feffd771b71fda72a
Charles maina <muragemaina09@gmail.com>
2019-04-01 07:47:41 +0300

    added a javascript page

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
-if(no1==="yes") {
-    return 2;
-}
-else if(no2==="stack") {
-    return 2;
-}
-else if(no3==="netscape") {
-    return 2;
-}
-else if(no4==="else") {
-    return 2;
-}
-else if(no5==="appear") {
-    return 2;
-}
-else if(no6==="yes") {
-    return 2;
-}
-else if(no7==="=") {
-    return 2;
-}
-else if(no8==="area") {
-    return 2;
-}
-else if(no9==="logical error") {
-    return 2;
-}
-else if(no10==="no") {
-    return 2;
-}
-else {
-    return 0;
-}
-
-var result=function(no1, no2, no3, no4, no5, no6, no7, no8, no9, no10); {
-return no1+no2+no3+no4+no5+no6+no7+no8+no9+no10;
-}
+$(document).ready(function () {
+    $(".button").click(function() {
+      $("#questions").slideToggle();
+    });
+  $("#button").click(function() {
+      var ans1 =parseInt($('input[name="no1"]:checked').val());
+      var ans2 =parseInt($('input[name="no2"]:checked').val());
+      var ans3 = parseInt($('input[name="no3"]:checked').val());
+      var ans4 = parseInt($('input[name="no4"]:checked').val());
+      var ans5 = parseInt($('input[name="no5"]:checked').val());
+      var score = 0;
+      if (ans1 == 10) {
+        score += 10;
+      }
+      if (ans2 ==10) {
+        score += 10;
+      }
+      if (ans3 == 10) {
+        score += 10;
+      }
+      if (ans4 ==10) {
+        score += 10;
+      }
+      if (ans5 == 10) {
+        score += 10;
+      }
+  $("#result").val("Your score is "+score);
+  $("#button").click(function () {
+    $("#questions").hide();
+    $("#result").slideToggle();
+  })
+  
+  });
+  });
+  
